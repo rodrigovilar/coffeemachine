6a0fb09db6747061d834da84db35b368d7919506
rodrigovilar <rvilar@gmail.com>
2014-06-01 18:23:02 -0300

    Creating project

---
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/FrontPanel.java
@@ -0,0 +1,4 @@
+package br.ufpb.dce.aps.coffeemachine;
+
+public class FrontPanel {
+}
---
+++ src/test/java/br/ufpb/dce/aps/coffeemachine/FrontPanelTest.java
@@ -0,0 +1,11 @@
+package br.ufpb.dce.aps.coffeemachine;
+
+import org.junit.Test;
+
+
+public class FrontPanelTest {
+
+	@Test
+	public void testApp() {
+	}
+}
